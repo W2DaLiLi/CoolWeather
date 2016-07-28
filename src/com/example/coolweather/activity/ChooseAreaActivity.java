2f407002e59a287e030e7d19ce320a396340dbf1
Yuwenhao <13277250082@163.com>
2016-07-28 21:11:51 +0800

    新增切换城市和手动更新天气功能。

--- src/com/example/coolweather/activity/ChooseAreaActivity.java
+++ src/com/example/coolweather/activity/ChooseAreaActivity.java
@@ -48,10 +48,13 @@ public class ChooseAreaActivity extends Activity {
 	private City selectedCity;//选中城市
 	private int currentLevel;//当前选中的级别
 	
+	private boolean isFromWeatherActivity;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-		if(prefs.getBoolean("city_selected", false)){
+		if(prefs.getBoolean("city_selected", false) &&!isFromWeatherActivity){
 			Intent intent = new Intent(this, WeatherActivity.class);
 			startActivity(intent);
 			finish();
@@ -208,6 +211,10 @@ public class ChooseAreaActivity extends Activity {
 		}else if(currentLevel == LEVEL_CITY){
 			queryProvinces();
 		}else{
+			if(isFromWeatherActivity){
+				Intent intent = new Intent(this,WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
 	}
--- src/com/example/coolweather/activity/WeatherActivity.java
+++ src/com/example/coolweather/activity/WeatherActivity.java
@@ -6,16 +6,19 @@ import com.example.coolweather.util.HttpUtil;
 import com.example.coolweather.util.Utility;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class WeatherActivity extends Activity {
+public class WeatherActivity extends Activity implements OnClickListener{
 	
 	private LinearLayout weatherInfoLayout;
 	
@@ -31,6 +34,10 @@ public class WeatherActivity extends Activity {
 	
 	private TextView currentDateText;
 	
+	private Button switchCity;
+	
+	private Button refreshWeather;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -42,6 +49,10 @@ public class WeatherActivity extends Activity {
 		temp1Text = (TextView) findViewById (R.id.temp1);
 		temp2Text = (TextView) findViewById (R.id.temp2);
 		currentDateText = (TextView) findViewById (R.id.current_data);
+		switchCity = (Button) findViewById (R.id.switch_city);
+		refreshWeather = (Button) findViewById (R.id.refresg_weather);
+		switchCity.setOnClickListener( this);
+		refreshWeather.setOnClickListener( this);
 		String countyCode = getIntent().getStringExtra("county_code");
 		if(!TextUtils.isEmpty(countyCode)){
 			publishText.setText("同步中...");
@@ -110,5 +121,25 @@ public class WeatherActivity extends Activity {
 		cityNameText.setVisibility(View.VISIBLE);
 	}
 	
+	public void onClick(View v){
+		switch (v.getId()){
+		case R.id.switch_city:
+			Intent intent = new Intent(this,ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresg_weather:
+			publishText.setText("同步中...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if(!TextUtils.isEmpty(weatherCode)){
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+			default:
+				break;
+		}
+	}
 	
 }
